444f8c0aa8adc03855edd84bac13f6807326bb99
Fragile Kharlyne Ngamewe Nkwekam <fragilengamewe@gmail.com>
2026-04-07 23:50:51 +0200

    maj articleData

--- frontend/src/pages/articlesData.js
+++ frontend/src/pages/articlesData.js
@@ -22,9 +22,21 @@ export const articlesData = [
   date: "15 Mars 2026",
   title: "Promouvoir l'inclusion",
   excerpt:
-    "Construire une école inclusive, c’est permettre à chaque enfant de trouver sa place, d’apprendre à son rythme et d’évoluer dans un environnement bienveillant, respectueux des différences.",
-  fullContent:
-    "L’école inclusive repose sur une idée essentielle : chaque enfant, quelles que soient ses particularités, doit pouvoir apprendre, participer et s’épanouir dans un cadre adapté à ses besoins. Pour les enfants autistes et plus largement les enfants en situation de handicap ou de neurodiversité, cela implique une meilleure compréhension de leurs réalités au quotidien.\n\nUne école inclusive ne signifie pas seulement accueillir la différence, mais aussi mettre en place des conditions concrètes pour favoriser la réussite de tous. Cela peut passer par des aménagements pédagogiques, un dialogue constant entre les familles et les professionnels, une sensibilisation des autres élèves, ainsi qu’une écoute attentive des besoins spécifiques de l’enfant.\n\nL’inclusion scolaire se construit aussi grâce à la coopération. Enseignants, parents, accompagnants et direction doivent pouvoir travailler ensemble afin de créer un environnement rassurant, stimulant et respectueux. Lorsqu’un enfant se sent compris, soutenu et valorisé, il peut développer sa confiance, ses compétences et son autonomie.\n\nPromouvoir l’inclusion à l’école, c’est enfin transmettre une valeur essentielle à tous les élèves : apprendre à vivre ensemble, dans le respect des différences, de la diversité et de la dignité de chacun.",
+    "Promouvoir l’inclusion, c’est permettre à chaque enfant de se sentir accepté, d’échanger et de partager des moments avec d’autres, quels que soient leurs besoins ou particularités.",
+  fullContent: [
+    {
+      type: "paragraph",
+      text:"Promouvoir l’inclusion, c’est permettre à chaque enfant de se sentir accepté, d’échanger et de partager des moments avec d’autres, quels que soient leurs besoins ou particularités. Notre objectif n’est pas de créer des classes inclusives à l’école, mais de favoriser des espaces et des activités où tous les enfants et leurs familles peuvent se rencontrer et partager des expériences enrichissantes. "
+    },
+     {
+      type: "paragraph",
+      text:"Par exemple, le 27 juin, nous organisons une marche détente inclusive, ouverte aux enfants à besoins spécifiques et neurotypiques, ainsi qu’à leurs familles et aux citoyens sensibles à l’autisme. Cette activité vise à offrir un moment agréable de partage, de rencontre et d’échanges, avec quelques animations pour permettre aux enfants de se côtoyer, de jouer et de mieux se comprendre."
+    },
+     {
+      type: "paragraph",
+      text:"D’autres événements viendront compléter cette démarche tout au long de l’année, afin de promouvoir l’inclusion dans la vie quotidienne et de créer des occasions concrètes où chacun peut se sentir valorisé et accueilli. "
+    }
+  ] ,
   image: techEdu,
   articleImage: techEdu,
   category: "Projet"
@@ -32,8 +44,8 @@ export const articlesData = [
   {
     id: 3,
     date: "15 Mars 2026",
-    title: "2025 : une année fondatrice pour notre engagement",
-    excerpt: "L’année 2025 a marqué une étape décisive dans la structuration de notre fondation, à travers des actions de sensibilisation, des témoignages, un travail de terrain et la préparation de notre lancement officiel prévu le 2 avril 2026.",
+    title: "2025 : une année prépatatoire pour notre projet",
+    excerpt: "En 2025, nous avons commencé à poser les bases de notre action, en nous engageant progressivement dans la sensibilisation à l’autisme.",
      fullContent: [
     {
       type: "paragraph",
@@ -41,54 +53,27 @@ export const articlesData = [
     },
     {
       type: "paragraph",
-      text: "En tant que collectif de parents, nous avons mené un travail de fond, à la fois humain et de terrain, afin de mieux comprendre les réalités vécues et d’orienter nos actions de manière concrète et pertinente."
-    },
-    {
-      type: "paragraph",
-      text: "Plusieurs initiatives ont vu le jour :"
+      text: "En tant que collectif de parents, nous avons initié des actions concrètes pour faire connaître les réalités vécues par les enfants et leurs familles :"
     },
     {
       type: "list",
       items: [
-        "Des actions de sensibilisation à travers des vidéos et témoignages diffusés sur nos réseaux",
-        "Une interview marquante retraçant le parcours d’une maman, du diagnostic à l’acceptation",
-        "La mobilisation de personnes alliées à notre cause, qui ont contribué à relayer et amplifier notre message",
-        "La création de contenus par des groupes d’enfants, donnant une parole authentique et précieuse"
+        "La réalisation et la diffusion de vidéos de sensibilisation, comprenant le témoignage marquant d’une maman retraçant son parcours, du diagnostic à l’acceptation. Cette vidéo est disponible sur notre chaîne YouTube Skye Foundation. (VIDEO1 TEMOIGNAGE D'UE MAMAN VIVANT AVEC UN ENFANT AUTISTE)",
+        "La participation de jeunes enfants et de personnes volontaires dans la création de contenus vidéos pour sensibiliser à l’autisme. Toutes ces vidéos peuvent être visionnées sur notre site et sur YouTube Skye Foundation. ",
+        "La préparation d’une bande dessinée basée sur les mots et ressentis des enfants, destinée à devenir un outil de sensibilisation accessible à tous, et qui sera mise à disposition sur notre site. ",
       ]
     },
     {
       type: "paragraph",
-      text: "Dans cette dynamique, une bande dessinée a été réalisée à partir des mots et ressentis des enfants, afin de proposer un outil de sensibilisation accessible à tous. Celle-ci sera mise à disposition sur notre site pour une utilisation libre dans un objectif de sensibilisation."
-    },
-        {
-      type: "paragraph",
-      text: "En parallèle, nous avons apporté un soutien, de manière officieuse, à une initiative au Cameroun. Cette expérience nous a permis d’observer, d’apprendre et d’approfondir notre compréhension du terrain, tant au niveau local qu’international."
-    },
-       {
-      type: "paragraph",
-      text: "Ce travail d’investigation, mené depuis plusieurs années en tant que parents engagés, nous a permis de poser des bases solides, d’identifier les besoins réels et de définir des actions que nous pouvons concrètement mettre en place."
+      text: "Ces actions, menées de manière officieuse, nous ont permis de prendre contact avec différentes personnes et de mieux comprendre comment structurer notre futur projet."
     },
     {
       type: "paragraph",
-      text: "Aujourd’hui, forts de ce parcours, nous sommes prêts à franchir une nouvelle étape."
-    },
-    {
+      text: "Aujourd’hui, forts de cette première expérience, nous sommes prêts à franchir une nouvelle étape : le lancement officiel de la fondation, prévu le 2 avril 2026."
+    },       {
       type: "paragraph",
-      text: "Le lancement officiel de la fondation est prévu le 2 avril 2026."
-    },
-    {
-      type: "paragraph",
-      text: "Nous remercions chaleureusement toutes les personnes qui nous ont déjà soutenus et accompagnés dans cette aventure."
-    },
-    {
-      type: "paragraph",
-      text: "Et pour celles et ceux qui souhaitent s’engager à nos côtés, soutenir nos actions ou simplement en savoir plus : vous êtes les bienvenus."
-    },
-    {
-      type: "paragraph",
-      text: "Ce travail d’investigation, mené depuis plusieurs années en tant que parents engagés, nous a permis de poser des bases solides, d’identifier les besoins réels et de définir des actions que nous pouvons concrètement mettre en place."
+      text: "Nous remercions chaleureusement toutes les personnes qui nous ont déjà soutenus et accompagnés dans cette aventure. Et pour celles et ceux qui souhaitent s’engager à nos côtés, soutenir nos actions ou simplement en savoir plus : vous êtes les bienvenus"
     }
-    
   ],
     articleImage: bilan, // Remplace par ton image HD dédiée si nécessaire
     image:bilan,
